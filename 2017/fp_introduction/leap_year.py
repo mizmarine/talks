3097035030584b9c50be7c1d9c06a88fa522dcef
vg-1000ch <Masahiko_Izumi@voyagegroup.com>
2017-03-22 17:44:49 +0900

    separate scritps

--- 2017/fp_introduction/leap_year.py
+++ 2017/fp_introduction/leap_year.py
@@ -1,19 +1,6 @@
 # -*- coding: utf-8 -*-
 
 
-def is_leap_year_naive(n):
-    if n % 4 == 0:
-        if n % 100 == 0:
-            if n % 400 == 0:
-                return True
-            else:
-                return False
-        else:
-            return True
-    else:
-        return False
-
-
 def is_leap_year_fast_return(n):
     if n % 400 == 0:
         return True
---
+++ 2017/fp_introduction/leap_year_naive.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+
+def is_leap_year_naive(n):
+    if n % 4 == 0:
+        if n % 100 == 0:
+            if n % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
